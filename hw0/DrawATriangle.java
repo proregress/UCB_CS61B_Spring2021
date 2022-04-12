93a79c17e4ed1d38602fc8e44f08c745ea7ff126
proregress <37496223+proregress@users.noreply.github.com>
2022-04-12 23:57:39 +0800

    Create DrawATriangle.java

---
+++ hw0/DrawATriangle.java
@@ -0,0 +1,18 @@
+public class DrawATriangle {
+    public static void main(String[] args) {
+        int col = 0;
+        int row = 0;
+        int SIZE = 5;
+        while (row <= SIZE) {
+            row = row + 1;
+            col = 0;
+            while (col < row) {
+                col = col + 1;
+                System.out.print('*');
+            }
+            System.out.println();
+
+        }
+
+    }
+}
