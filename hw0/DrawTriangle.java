3e4e1de0c7da46a662e105920a657ab5797e3dbd
proregress <37496223+proregress@users.noreply.github.com>
2022-04-15 23:44:48 +0800

    Create DrawTriangle.java
    Creative Exercise 1b

---
+++ hw0/DrawTriangle.java
@@ -0,0 +1,22 @@
+// Creative Exercise 1b
+
+public class DrawTriangle {
+   public static void drawTriangle(int N){
+      int col = 0;
+      int row = 0;
+      while (row < N) {
+          row = row + 1;
+          col = 0;
+          while (col < row) {
+              col = col + 1;
+              System.out.print('*');
+          }
+          System.out.println();
+      }
+   }
+   
+   public static void main(String[] args) {
+      System.out.println(drawTriangle(10));
+      
+   }
+}
